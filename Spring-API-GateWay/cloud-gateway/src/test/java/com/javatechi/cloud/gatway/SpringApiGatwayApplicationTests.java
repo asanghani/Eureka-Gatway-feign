e58a538899e7f5b851c5dc8a86a67c1125092f21
asanghani <sanghanialpesh@gmail.com>
2020-09-21 15:34:46 -0500

    cloud gatway added
    cloud gateway added

---
+++ Spring-API-GateWay/cloud-gateway/src/test/java/com/javatechi/cloud/gatway/SpringApiGatwayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.javatechi.cloud.gatway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringApiGatwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
